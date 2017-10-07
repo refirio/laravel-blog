18d4133b823f5da22e01594a481c2b74cc4230f2
refirio <refirio@gmail.com>
2017-10-07 23:59:19 +0900

    マイグレーションを調整

--- database/migrations/2017_10_07_234515_create_tags_table.php
+++ database/migrations/2017_10_07_234515_create_tags_table.php
@@ -15,8 +15,19 @@ class CreateTagsTable extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name')->comment('タグ');
             $table->timestamps();
+            $table->softDeletes();
         });
+
+        Schema::create('entry_tag', function(Blueprint $table)
+        {
+            $table->integer('entry_id')->unsigned()->index();
+            $table->integer('tag_id')->unsigned()->index();
+
+            $table->foreign('entry_id')->references('id')->on('entries')->onDelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+         });
     }
 
     /**
@@ -27,5 +38,6 @@ class CreateTagsTable extends Migration
     public function down()
     {
         Schema::dropIfExists('tags');
+        Schema::dropIfExists('entry_tag');
     }
 }
